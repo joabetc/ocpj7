3d94b7900b6bc2e7180422e2fb5834e9b6fd0f70
Joabe Costa <joabetc@hotmail.com>
2018-05-19 19:22:16 -0300

    Creating the file

--- io_nio/Writer1.java
+++ io_nio/Writer1.java
@@ -2,6 +2,13 @@ import java.io.*;
 
 class Writer1 {
   public static void main(String[] args) {
-    File file = new File("fileWriter1.txt");
+    try {
+      boolean newFile = false;
+      File file = new File("fileWriter1.txt");
+      System.out.println(file.exists());
+      newFile = file.createNewFile();
+      System.out.println(newFile);
+      System.out.println(file.exists());
+    } catch(IOException e) { }
   }
 }
