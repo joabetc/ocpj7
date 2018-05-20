5f750be3cd2efd9711779a8ea844e02aeffb52b2
Joabe Costa <joabetc@hotmail.com>
2018-05-20 19:12:16 -0300

    Changing sample

--- io_nio/CopyMoveDelete.java
+++ io_nio/CopyMoveDelete.java
@@ -4,13 +4,13 @@ import java.io.*;
 class CopyMoveDelete {
   public static void main(String[] args) {
     try {
-      Path source = Paths.get("temp/test1");
-      Path target = Paths.get("temp/test2.txt");
-      Files.copy(source, target);
-      Files.delete(target);
-      Files.move(source, target);
+      Path one = Paths.get("mydir/myFile.txt");
+      Path two = Paths.get("mydir/myFile0.txt");
+      Path targ = Paths.get("mydir/test3.txt");
+      Files.copy(one, targ);
+      Files.copy(two, targ);
     } catch (IOException e) {
 
-    }    
+    }
   }
 }
