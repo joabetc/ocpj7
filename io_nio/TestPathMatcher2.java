bfc148b39a2bf8d70ead3f16e6a601d2d8f08290
Joabe Costa <joabetc@hotmail.com>
2018-05-23 20:59:56 -0300

    Even more glob examples

--- io_nio/TestPathMatcher2.java
+++ io_nio/TestPathMatcher2.java
@@ -2,12 +2,11 @@ import java.nio.file.*;
 
 class TestPathMatcher2 {
   public static void main(String[] args) {
-    Path path1 = Paths.get("One.java");
-    Path path2 = Paths.get("One.ja^a");
-    matches(path1, "glob:*.????");
-    matches(path1, "glob:*.???");
-    matches(path2, "glob:*.????");
-    matches(path2, "glob:*.???");
+    Path path1 = Paths.get("Bert-book");
+    Path path2 = Paths.get("Kathy-horse");
+    matches(path1, "glob:{Bert*,Kathy*}");
+    matches(path1, "glob:{Bert,Kathy}*");
+    matches(path2, "glob:{Bert,Kathy}");
   }
 
   public static void matches(Path path, String glob) {
