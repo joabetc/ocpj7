97a85a64c74742dc6ac2ba5c05d21fae5ac33935
Joabe Costa <joabetc@hotmail.com>
2018-05-23 21:06:04 -0300

    Tricky example with glob

--- io_nio/TestPathMatcher2.java
+++ io_nio/TestPathMatcher2.java
@@ -2,11 +2,15 @@ import java.nio.file.*;
 
 class TestPathMatcher2 {
   public static void main(String[] args) {
-    Path path1 = Paths.get("Bert-book");
-    Path path2 = Paths.get("Kathy-horse");
-    matches(path1, "glob:{Bert*,Kathy*}");
-    matches(path1, "glob:{Bert,Kathy}*");
-    matches(path2, "glob:{Bert,Kathy}");
+    Path path1 = Paths.get("0*b/test/1");
+    Path path2 = Paths.get("9\\*b/test/1");
+    Path path3 = Paths.get("01b/test/1");
+    Path path4 = Paths.get("0*b/1");
+    String glob = "glob:[0-9]\\*{A*,b}/**/1";
+    matches(path1, glob);
+    matches(path2, glob);
+    matches(path3, glob);
+    matches(path4, glob);
   }
 
   public static void matches(Path path, String glob) {
