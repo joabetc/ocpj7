5a3f45133b5da2e9dd1747126a062f5abf205f63
Joabe Costa <joabetc@hotmail.com>
2018-05-26 16:20:01 -0300

    Sorting ArrayList with Collections

---
+++ generics_collections/TestSort1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class TestSort1 {
+  public static void main(String[] args) {
+    ArrayList<String> stuff = new ArrayList<String>();
+    stuff.add("Denver");
+    stuff.add("Boulder");
+    stuff.add("Vail");
+    stuff.add("Aspen");
+    stuff.add("Telluride");
+    System.out.println("unsorted " + stuff);
+    Collections.sort(stuff);
+    System.out.println("sorted   " + stuff);
+  }
+}
