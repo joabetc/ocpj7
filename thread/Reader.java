9da27c906ec4f50179da47603b9c5a91f4c8579c
Joabe Costa <joabetc@hotmail.com>
2018-05-27 19:46:35 -0300

    Using notifyAll() When Many Threads May Be Waiting

---
+++ thread/Reader.java
@@ -0,0 +1,38 @@
+class Reader extends Thread {
+  Calculator c;
+
+  public Reader(Calculator calc) {
+    c = calc;
+  }
+
+  public void run() {
+    synchronized(c) {
+      try {
+        System.out.println("Waiting for calculation...");
+        c.wait();
+      } catch(InterruptedException e) {}
+      System.out.println("Total ia: " + c.total);
+    }
+  }
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    new Reader(calculator).start();
+    new Reader(calculator).start();
+    new Reader(calculator).start();
+    new Thread(calculator).start();
+  }
+}
+
+class Calculator implements Runnable {
+  int total;
+
+  public void run() {
+    synchronized(this) {
+      for(int i = 0; i < 100; i++) {
+        total += i;
+      }
+      notifyAll();
+    }
+  }
+}
