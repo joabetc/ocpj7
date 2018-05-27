47c398e29f2ac52fa7622731ae49bef182a7f9cc
Joabe Costa <joabetc@hotmail.com>
2018-05-27 12:12:49 -0300

    Accessing a final local variable within a method-local inner class

--- inner_classes/MyOuter2.java
+++ inner_classes/MyOuter2.java
@@ -2,8 +2,10 @@ class MyOuter2 {
   private String x = "Outer2";
   void doStuff() {
     class MyInner {
+      final String z = "local variable";
       public void seeOuter() {
         System.out.println("Outer x is " + x);
+        System.out.println("Local var z is " + z);
       }
     }
 
