5a375195993c8091e265702e56f6ba1ffccb8c34
Joabe Costa <joabetc@hotmail.com>
2018-06-10 20:35:50 -0300

    How long it took for a trillion milliseconds to pass, starting at January 1, 1970

---
+++ data_formating/TestDates.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class TestDates {
+  public static void main(String[] args) {
+    Date d1 = new Date(1_000_000_000_000L);
+    System.out.println("1st date " + d1.toString());
+  }
+}
