ee748fd7d3c87cdb9e898c8fc91c30b0aef39475
Joabe Costa <joabetc@hotmail.com>
2018-06-10 20:37:51 -0300

    Adding an hour to Date instance using setTime and getTime

--- data_formating/TestDates.java
+++ data_formating/TestDates.java
@@ -3,5 +3,7 @@ class TestDates {
   public static void main(String[] args) {
     Date d1 = new Date(1_000_000_000_000L);
     System.out.println("1st date " + d1.toString());
+    d1.setTime(d1.getTime() + 3_600_000);
+    System.out.println("new time " + d1.toString());
   }
 }
