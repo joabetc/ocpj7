964f21c427012d13a643bd1017a9293fb8fb8519
Joabe Costa <joabetc@hotmail.com>
2018-06-13 21:38:55 -0300

    Testing some NumberFormat methods

---
+++ data_formating/TestNumberFormat2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.text.*;
+
+public class TestNumberFormat2 {
+  public static void main(String[] args) {
+    float f1 = 123.45678f;
+    NumberFormat nf = NumberFormat.getInstance();
+    System.out.print(nf.getMaximumFractionDigits() + " ");
+    System.out.print(nf.format(f1) + "  ");
+
+    nf.setMaximumFractionDigits(5);
+    System.out.println(nf.format(f1) + "  ");
+
+    try {
+      System.out.println(nf.parse("1234.567"));
+      nf.setParseIntegerOnly(true);
+      System.out.println(nf.parse("1234.567"));
+    } catch (ParseException pe) {
+      System.out.println("parse exc");
+    }
+  }
+}
