5d65e3fd9170ed055471f63671b3f30c7c0c3948
Joabe Costa <joabetc@hotmail.com>
2018-06-13 21:30:32 -0300

    The NumberFormat Class

---
+++ data_formating/TestNumberFormat.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.text.*;
+
+public class TestNumberFormat {
+  public static void main(String[] args) {
+    float f1 = 123.4567f;
+    Locale locFR = new Locale("fr");
+    NumberFormat[] nfa = new NumberFormat[4];
+
+    nfa[0] = NumberFormat.getInstance();
+    nfa[1] = NumberFormat.getInstance(locFR);
+    nfa[2] = NumberFormat.getCurrencyInstance();
+    nfa[3] = NumberFormat.getCurrencyInstance(locFR);
+
+    for(NumberFormat nf : nfa) {
+      System.out.println(nf.format(f1));
+    }
+  }
+}
