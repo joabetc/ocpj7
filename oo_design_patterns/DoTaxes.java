2a517aa77191f39817e949b0a51514e53bde3b4a
Joabe Costa <joabetc@hotmail.com>
2018-07-01 19:12:01 -0300

    Example of tight coupling

---
+++ oo_design_patterns/DoTaxes.java
@@ -0,0 +1,17 @@
+public class DoTaxes {
+  float rate;
+  float doColorado() {
+    SalesTaxRates str = new SalesTaxRates();
+    rate = str.salesRate; // this should be a method call like:
+  }                       // rate = str.getSalesRate("CO");
+}
+
+class SalesTaxRates {
+  public float salesRate; // should be private
+  public float adjustedSalesRate; // should be private
+
+  public float getSalesRate(String region) {
+    salesRate = new DoTaxes().doColorado();
+    return adjustedSalesRate;
+  }
+}
