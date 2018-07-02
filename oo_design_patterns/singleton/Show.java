b14858f795e7346f7acafa981783b40dd3d4baab
Joabe Costa <joabetc@hotmail.com>
2018-07-02 10:22:48 -0300

    The problem

---
+++ oo_design_patterns/singleton/Show.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Show {
+
+  private Set<String> availableSeats;
+
+  public Show() {
+    availableSeats = new HashSet<String>();
+    availableSeats.add("1A");
+    availableSeats.add("1B");
+  }
+
+  public boolean bookSeat(String seat) {
+    return availableSeats.remove(seat);
+  }
+
+  public static void main(String[] args) {
+    ticketAgentBooks("1A");
+    ticketAgentBooks("1A");
+  }
+
+  private static void ticketAgentBooks(String seat) {
+    Show show = new Show();
+    System.out.println(show.bookSeat(seat));
+  }
+}
