cfbed72a04caf068cee9752df8a01fe8958bf930
Joabe Costa <joabetc@hotmail.com>
2018-07-21 20:01:50 -0300

    ArrayList Basics

---
+++ generics_collections/ArrayListBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArrayListBasics {
+  public static void main(String[] args) {
+    List<String> test = new ArrayList<String>();
+    String s = "hi";
+    test.add("string");
+    test.add(s);
+    test.add(s+s);
+    System.out.println(test.size());
+    System.out.println(test.contains(42));
+    System.out.println(test.contains("hihi"));
+    test.remove("hi");
+    System.out.println(test.size());
+  }
+}
