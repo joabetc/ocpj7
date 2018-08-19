2ee4c6bec71feef249d869f15205dac5bca81e4d
Joabe Costa <joabetc@hotmail.com>
2018-08-19 11:34:45 -0300

    Using view to update file attributes

--- io_nio/WorkingWithDosFileAttributes.java
+++ io_nio/WorkingWithDosFileAttributes.java
@@ -7,13 +7,18 @@ public class WorkingWithDosFileAttributes {
     try {
       Path path = Paths.get("test");
       Files.createFile(path);
-      Files.setAttribute(path, "dos:hidden", true);
-      Files.setAttribute(path, "dos:readonly", true);
+      //Files.setAttribute(path, "dos:hidden", true);
+      //Files.setAttribute(path, "dos:readonly", true);
+      DosFileAttributeView view = Files.getFileAttributeView(path, DosFileAttributeView.class);
+      view.setHidden(true);
+      view.setReadOnly(true);
       DosFileAttributes dos = Files.readAttributes(path, DosFileAttributes.class);
       System.out.println(dos.isHidden());
       System.out.println(dos.isReadOnly());
-      Files.setAttribute(path, "dos:hidden", false);
-      Files.setAttribute(path, "dos:readonly", false);
+      //Files.setAttribute(path, "dos:hidden", false);
+      //Files.setAttribute(path, "dos:readonly", false);
+      view.setHidden(false);
+      view.setReadOnly(false);
       dos = Files.readAttributes(path, DosFileAttributes.class);
       System.out.println(dos.isHidden());
       System.out.println(dos.isReadOnly());
