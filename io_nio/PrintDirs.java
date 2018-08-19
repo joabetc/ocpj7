368ec7706d8671b899ca4f298abf5e32dd45670e
Joabe Costa <joabetc@hotmail.com>
2018-08-19 12:44:18 -0300

    Using methods from SimpleFileVisitor

---
+++ io_nio/PrintDirs.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+
+public class PrintDirs extends SimpleFileVisitor<Path> {
+  public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+    System.out.println("pre: " + dir);
+    return FileVisitResult.CONTINUE;
+  }
+  public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+    System.out.println("file: " + file);
+    return
+    FileVisitResult.CONTINUE;
+  }
+  public FileVisitResult visitFileFailed(Path file, IOException exc) {
+    return FileVisitResult.CONTINUE;
+  }
+  public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+    System.out.println("post: " + dir);
+    return FileVisitResult.CONTINUE;
+  }
+  public static void main(String[] args) {
+    try {
+      PrintDirs dirs = new PrintDirs();
+      Files.walkFileTree(Paths.get("C:/Users/Joabe/Projetos/ocpj7/io_nio"), dirs);
+    } catch (IOException e) {
+
+    }
+  }
+}
