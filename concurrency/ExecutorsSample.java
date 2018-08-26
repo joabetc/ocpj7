9832a74774c2ae768e5c81309d10bb5e8be3ad14
Joabe Costa <joabetc@hotmail.com>
2018-08-26 15:17:17 -0300

    Changing to Fixed Thread Pools and getting available processors

--- concurrency/ExecutorsSample.java
+++ concurrency/ExecutorsSample.java
@@ -1,10 +1,14 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.lang.Runtime;
 
 public class ExecutorsSample {
   public static void main(String[] args) {
+    Runtime rt = Runtime.getRuntime();
+    int cpus = rt.availableProcessors();
+    System.out.println(cpus);
     Runnable r = new MyRunnableTask();
-    ExecutorService ex = Executors.newCachedThreadPool();
+    ExecutorService ex = Executors.newFixedThreadPool(cpus);
     ex.execute(r);
   }
 }
